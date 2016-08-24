87acb25249f9b57340b99adc38104e01eef385e5
Amir <amir_rajabifar@yahoo.com.au>
2016-08-24 09:19:13 +0800

    Convert *.ino to *.c
    This completes exercise 1

---
+++ Ex1/Sketch_Self-Study_Lab1/Sketch_Self-Study_Lab1.ino
@@ -0,0 +1,19 @@
+#include <msp430fr5739.h>
+
+int dutyC = 90;             // Duty Cycle: Valid Range = 10 to 90
+
+void setup() {
+  WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+  P1DIR |= BIT2;            // P1.2 to output
+  P1SEL0 |= BIT2;	    // P1.2 to TA1.1
+ 
+  TA1CCR0 = 1000-1;	    // PWM period
+  TA1CCTL1 = OUTMOD_7;      // CCR1 reset/set
+  TA1CCR1 = dutyC * 10;	    // CCR1 PWM duty cycle
+  TA1CTL = TASSEL_2 + MC_1; // SMCLK, up mode
+ 
+  _BIS_SR(LPM0_bits);       // Enter LPM0
+}
+
+void loop() {
+}
--- Ex1/main.c
+++ Ex1/main.c
@@ -1,10 +1,29 @@
-#include <msp430.h> 
-
 /*
  * main.c
+ * Self study 1
+ * author: Amir Rajabifar <amir_rajabifar@uahoo.com.au>
+ * 
+ * Pulse With Mudulation (PWM)
+ * 
+ * Objective: convert the provided energia code (*.ino)
+ * to C code (*.c)
  */
+
+#include <msp430fr5739.h> 
+
+int dutyC = 90; //Duty cycle 10 to 90
+
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;	//Stop watchdog timer
 	
+	P1DIR |= BIT2; 				//Set P1.2 to OUTPUT
+	P1SEL0 |= BIT2;				//From P1.2 select TA1.1
+
+	TA1CCR0 = 1000-1;	    	// PWM period
+	TA1CCTL1 = OUTMOD_7;      	// CCR1 reset/set
+	TA1CCR1 = dutyC * 10;	    // CCR1 PWM duty cycle
+	TA1CTL = TASSEL_2 + MC_1; 	// SMCLK, up mode
+
+	_BIS_SR(LPM0_bits);       	// Enter LPM0
 	return 0;
 }
