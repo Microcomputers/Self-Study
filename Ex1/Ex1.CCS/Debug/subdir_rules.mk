3cfcc5eeabbcb89ef23778255d23a42af69374eb
Amir <amir_rajabifar@yahoo.com.au>
2016-08-24 09:53:29 +0800

    Add project files
    These are auto generated files from CCS IDE.
    (Makes life easier when opening the project in the IDE)

---
+++ Ex1/Ex1.CCS/Debug/subdir_rules.mk
@@ -0,0 +1,13 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS) $(GEN_HDRS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: MSP430 Compiler'
+	"/home/amir/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/bin/cl430" -vmspx --use_hw_mpy=F5 --include_path="/home/amir/ti/ccsv6/ccs_base/msp430/include" --include_path="/home/amir/ti/ccsv6/tools/compiler/msp430_15.12.2.LTS/include" --advice:power=all -g --define=__MSP430FR5739__ --diag_wrap=off --diag_warning=225 --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --printf_support=minimal --preproc_with_compile --preproc_dependency="main.d" $(GEN_OPTS__FLAG) "$(shell echo $<)"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
