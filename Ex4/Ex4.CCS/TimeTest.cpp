eb3fb6cd06b1771cfa2fa6c219712dafbeb1776e
Amir <amir_rajabifar@yahoo.com.au>
2016-10-10 12:15:35 +0800

    Add start() and end() implementation

--- Ex4/Ex4.CCS/TimeTest.cpp
+++ Ex4/Ex4.CCS/TimeTest.cpp
@@ -1,4 +1,5 @@
 #include <msp430fr5739.h>
+#include <inttypes.h>
 #include "TimeTest.h"
 
 void TimeTest::init ()
@@ -7,12 +8,49 @@ void TimeTest::init ()
 	TB0CTL = MC_2 | ID_0 | TASSEL_2;
 }
 
+//initialises each measurement
 void TimeTest::start ()
 {
-
+	oldtcnt = TB0R;
 }
 
 void TimeTest::end ()
 {
+	uint16_t newtcnt;
+ //If called before TimeTestStart leave now!!!
+ // If the tcnt value is zero and no previous count get out!
+	if (!((oldtcnt == 0)&&(count == 0))) 
+	{	
+		newtcnt = TB0R;
+		count += 1; // increment count
+		newtcnt -= (oldtcnt + TimeTestOffset);
+		totaltime += newtcnt;
+		if (count == 1) 
+		{ // If first time through
+			min = newtcnt;
+			max = newtcnt;
+			mincount = 1;
+			maxcount = 1;
+		}
+		else if( newtcnt < min)
+		{ // New minimum
+			min = newtcnt;
+			mincount = 1;
+		}
+		else if( newtcnt == min )
+		{
+			mincount++;
+		} //another at minimum
+		else if( newtcnt > max)
+		{ // New maximum
+			max = newtcnt;
+			maxcount = 1;
+		}
+		else if( newtcnt == max )
+		{
+			maxcount++; 
+		} // another at maximum
+	    else {}
+	}
 	
 }
--- Ex4/Ex4.CCS/TimeTest.h
+++ Ex4/Ex4.CCS/TimeTest.h
@@ -1,19 +1,19 @@
 #ifndef TIMETEST_H
 #define TIMETEST_H 
 
-#include <inttypes.h>
+#define TimeTestOffset (22)
 
 class TimeTest
 {
 private:
 	uint16_t oldtcnt;
 	uint16_t count;	
-	uint16_t main;	
+	uint16_t min;	
 	uint16_t mincount;	
 	uint16_t max;
 	uint16_t maxcount;
 	uint16_t totaltime;
-		
+
 public:
 	void init();		// test init
 	void start();		// test start
