b3caad5770c5a734b2cf7c3ccf2272933a20df6c
Amir <amir_rajabifar@yahoo.com.au>
2016-10-10 11:15:07 +0800

    Add project files & Start Timetest lib

---
+++ Ex4/Ex4.CCS/TimeTest.cpp
@@ -0,0 +1,17 @@
+#include "TimeTest.h"
+
+void TimeTest::init ()
+{
+	//continuous mode!!!!
+	TB0CTL = MC_2 | ID_0 | TASSEL_2;
+}
+
+void TimeTest::start ()
+{
+
+}
+
+void TimeTeset::end ()
+{
+	
+}
