ad956d3b9a8c801028f851c9bacd0fb660d4d8e6
Amir <amir_rajabifar@yahoo.com.au>
2016-10-10 11:26:07 +0800

    readd Ex4

---
+++ Ex4/Ex4.CCS/TimeTest.h
@@ -0,0 +1,15 @@
+#ifndef TIMETEST_H
+#define TIMETEST_H 
+
+
+class TimeTest
+{
+	
+public:
+	void init();		// test init
+	void start();		// test start
+	void end();			// test end
+	
+};
+
+#endif
---
+++ Ex4/Ex4.CCS/main.cpp
@@ -0,0 +1,11 @@
+#include <.h>
+#include "TimeTest.h"
+int main()
+{
+	TimeTest timer1; //Declare Timer1 of TimeTest
+	while (1)
+	{
+
+	}
+	return 0;
+}
