4d87b58059ad85a79cc237d00e840bc950bacae9
Amir <amir_rajabifar@yahoo.com.au>
2016-10-31 10:27:48 +0800

    Works with accel only

--- Ex2/Ex2.main/system.c
+++ Ex2/Ex2.main/system.c
@@ -1,231 +1,92 @@
-#include "msp430fr5739.h"
 #include "system.h"
+#include <msp430fr5739.h> 
 
-volatile unsigned char ThreshRange[3]={0,0,0};
-extern unsigned int CalValue_t, CalValue_x, CalValue_y, CalValue_z;
+unsigned int ADC_counter = 0;
+unsigned int ADCResult_X, ADCResult_Y, ADCResult_Z;
+unsigned int CalValue_X, CalValue_Y, CalValue_Z;
 
-void ledsInit(uc_8 ledNumber)
+void systemInit(void)
 {
-	uc_8 temp = 1 << (ledNumber - 1);
+	//Startup clock system in max. DCO setting ~8MHz
+	// This value is closer to 10MHz on untrimmed parts  
+	CSCTL0_H = 0xA5;                          // Unlock register
+	CSCTL1 |= DCOFSEL0 + DCOFSEL1;            // Set max. DCO setting
+	CSCTL2 = SELA_1 + SELS_3 + SELM_3;        // set ACLK = vlo; MCLK = DCO
+	CSCTL3 = DIVA_0 + DIVS_0 + DIVM_0;        // set all dividers 
+	CSCTL0_H = 0x01;                          // Lock Register
+  
+  	// Turn off temp. 
+  	REFCTL0 |= REFTCOFF; 
+  	REFCTL0 &= ~REFON;
+  	
+	// Enable LEDs
+	P3OUT &= ~(BIT6+BIT7+BIT5+BIT4);
+	P3DIR |= BIT6+BIT7+BIT5+BIT4;
+	PJOUT &= ~(BIT0+BIT1+BIT2+BIT3);
+	PJDIR |= BIT0 +BIT1+BIT2+BIT3;  
 
-	uc_8 PJMASK = (uc_16)temp & 0x000F;
-	uc_8 P3MASK = temp & 0xF0;
-
-	PJOUT &= ~PJMASK;
-	P3OUT &= ~P3MASK;
-
-	PJDIR |= PJMASK;
-	P3DIR |= P3MASK;
+  	// P3.0,P3.1 and P3.2 are accelerometer inputs
+  	P3OUT &= ~(BIT0 + BIT1 + BIT2);   
+  	P3DIR &= ~(BIT0 + BIT1 + BIT2); 
+  	P3REN |= BIT0 + BIT1 + BIT2;	
 }
 
-bool ledsWrite(uc_8 ledNumber, uc_8 value)
-{
-	uc_8 temp = 1 << (ledNumber - 1);
-
-	uc_8 PJMASK = (uc_16)temp & 0x000F;
-	uc_8 P3MASK = temp & 0xF0;
-
-	if (value != 0){
-		PJOUT |= PJMASK;
-		P3OUT |= P3MASK;
-	} else {
-		PJOUT &= ~PJMASK;
-		P3OUT &= ~P3MASK;
-	}
-
-	return TRUE;
-}
-
-void switchesInit(uc_8 buttonNumber)
-{
-	// Enable on-board switches
-	uc_8 temp = 1 << (buttonNumber - 1);
-	uc_8 P4MASK = temp & 0x03;
-	P4OUT |= P4MASK;			// Configure pullup resistor
-	P4DIR &= ~(P4MASK);		// Direction = input
-	P4REN |= P4MASK;			// Enable pullup resistor
-/*
-	P4IES &= ~(BIT0 + BIT1);		// P4.0 and P4.1 Lo/Hi edge interrupts
-	P4IE = BIT0 + BIT1;				// P4.0 and P4.1 interrupts enabled
-	P4IFG = 0;						// P4 IFG cleared
-	*/
-}
-
-//reade switch values, (digitalRead())
-bool switchesRead(uc_8 buttonNumber, uc_8 *value)
-{
-	uc_8 temp = 1 << (buttonNumber - 1);
-	uc_8 P4MASK = temp & 0x03;
-
-	if(((~P4IN) & P4MASK) != 0)
-	{
-		*value = 1;
-	}
-	else
-	{
-		*value = 0;
-	}
-
-	return TRUE;
-}
-
-void SetupThermistor(void)
-{
-    // ~16KHz sampling
-    //Turn on Power
-    P2DIR |= BIT7;
-    P2OUT |= BIT7;
-    // Configure ADC
-    P1SEL1 |= BIT4;
-    P1SEL0 |= BIT4;
-    // Allow for settling delay
-    __delay_cycles(50000);
-    // Configure ADC
-    ADC10CTL0 &= ~ADC10ENC;
-    ADC10CTL0 = ADC10SHT_7 + ADC10ON;        // ADC10ON, S&H=192 ADC clks
-    // ADCCLK = MODOSC = 5MHz
-    ADC10CTL1 = ADC10SHS_0 + ADC10SHP + ADC10SSEL_0;
-    ADC10CTL2 = ADC10RES;                    // 10-bit conversion results
-    ADC10MCTL0 = ADC10INCH_4;                // A4 ADC input select; Vref=AVCC
-    ADC10IE = ADC10IE0;                      // Enable ADC conv complete interrupt
-    // Setup Thresholds for relative difference in Thermistor measurements
-    ThreshRange[0]=15;
-    ThreshRange[1]=25;
-    ThreshRange[2]=45;
-    __enable_interrupt();
-}
-
-void ShutDownTherm(void)
-{
-    // Turn off Vcc
-    P2DIR &= ~BIT7;
-    P2OUT &= ~BIT7;
-    // Turn off ADC Channel
-    P1SEL1 &= ~BIT4;
-    P1SEL0 &= ~BIT4;
-    // Turn off ADC
-    ADC10CTL0 &= ~(ADC10ENC + ADC10ON);
-    ADC10IE &= ~ADC10IE0;
-    ADC10IFG = 0;
-}
-
-// Enable switches interfaced to P4.0 and P4.1
-inline void EnableSwitches()
-{
-	P4IFG = 0;								// P4 IFG cleared
-	P4IE = BIT0 + BIT1;						// P4.0 and P4.1 interrupts enabled
-}
-
-// Disable switches interfaced to P4.0 and P4.1
-inline void DisableSwitches()
-{
-	P4IFG = 0;								// P4 IFG cleared
-	P4IE &= ~(BIT0 + BIT1);					// P4.0 and P4.1 interrupts disabled
-	P4IFG = 0;								// P4 IFG cleared
-}
-
-// Simple software switch debounce model as a constant delay
-inline void StartDebounceTimer(uc ucDelay)
-{
-	// Default delay = 0
-	TA1CCTL0 = CCIE;						// TACCR0 interrupt enabled
-	if(ucDelay)
-		TA1CCR0 = SW_HALF_DEB_CONST;
-	else
-		TA1CCR0 = SW_DEB_CONST;
-	TA1CTL = TASSEL_1 + MC_1;				// ACLK, up mode
-}
-
-void CalibrateADC(void)
+//Sets the "zero" point of the accelerometer
+void calibrateADC(void) 
 {
 	unsigned char CalibCounter =0;
 	unsigned int Value = 0;
-
-	// Disable interrupts & user input during calibration
-	DisableSwitches();
-	__disable_interrupt();
-
-	ADC10CTL0 &= ~ADC10ENC;		//Toggle ENC bit
-	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_4;	//Sample thermistor
-	while(CalibCounter < 50)
-	{
-		P3OUT ^= BIT4;
-		CalibCounter++;
-		ADC10CTL0 |= ADC10ENC | ADC10SC;    // Start conversion
-		while (ADC10CTL1 & BUSY); 			// wait for sample to be done
-		Value += ADC10MEM0;
-	}
-	CalValue_t = Value/50;
-	//reset the counters
-	CalibCounter = 0;
-	Value = 0;
-
-	ADC10CTL0 &= ~ADC10ENC;		//Toggle ENC bit
-	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;	//Sample thermistor
-	while(CalibCounter < 50)
-	{
-		P3OUT ^= BIT4;
-		CalibCounter++;
-		ADC10CTL0 |= ADC10ENC | ADC10SC;    // Start conversion
-		while (ADC10CTL1 & BUSY); 			// wait for sample to be done
-		Value += ADC10MEM0;
-	}
-	CalValue_x = Value/50;
-	//reset the counters
-	CalibCounter = 0;
-	Value = 0;
-
-	ADC10CTL0 &= ~ADC10ENC;		//Toggle ENC bit
-	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_13;	//Sample thermistor
-	while(CalibCounter < 50)
-	{
-		P3OUT ^= BIT4;
-		CalibCounter++;
-		ADC10CTL0 |= ADC10ENC | ADC10SC;    // Start conversion
-		while (ADC10CTL1 & BUSY); 			// wait for sample to be done
-		Value += ADC10MEM0;
-	}
-	CalValue_y = Value/50;
-	//reset the counters
-	CalibCounter = 0;
-	Value = 0;
-
-	ADC10CTL0 &= ~ADC10ENC;		//Toggle ENC bit
-	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_14;	//Sample thermistor
-	while(CalibCounter < 50)
-	{
-		P3OUT ^= BIT4;
-		CalibCounter++;
-		ADC10CTL0 |= ADC10ENC | ADC10SC;    // Start conversion
-		while (ADC10CTL1 & BUSY); 			// wait for sample to be done
-		Value += ADC10MEM0;
-	}
-	CalValue_z = Value/50;
-	//reset the counters
-	CalibCounter = 0;
-	Value = 0;
-
-	ADC10CTL0 &= ~ADC10ENC; // Toggle ENC bit. Need this to change the ADC10INCH_t value.
-	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_4 ; // WE NEED TO START AT THE X AXES FIRST DUE TO HOW THE INTERUPT ROUTINE WORKS.
-
-
-	// Reenable switches and interrupts after calibration
-	EnableSwitches();
-	__enable_interrupt();
-}
-
-void acceInit()
-{
-	// P3.0,p3.1 and P3.2 are accelerometer inputs
-	P3OUT &= ~(BIT0 + BIT1 + BIT2);
-	P3DIR &= ~(BIT0 + BIT1 + BIT2);
-	P3REN |= ~(BIT0 + BIT1 + BIT2);
 	
+    __disable_interrupt();	//Turn off any interupts just incase
+    
+    ADC10CTL0 &= ~ADC10ENC; //Toggle ENC bit. Need this to change the ADC10INCH_x value.
+    ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;	//Sample the X-axis  
+	while(CalibCounter <50)
+	{
+		P3OUT ^= BIT4;
+		CalibCounter++;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;	//Start sample.
+		while (ADC10CTL1 & BUSY);	//Wait for sample to be done.
+		Value += ADC10MEM0;
+	}
+	CalValue_X = Value/50;	//Average all samples to find the best zero.
+	
+	ADC10CTL0 &= ~ADC10ENC;	//Toggle ENC bit. Need this to change the ADC10INCH_x value.
+	CalibCounter = 0;	//Reset the counters
+	Value = 0;
+	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_13;	//Sample the Y-axis 
+	while(CalibCounter <50)
+	{
+		P3OUT ^= BIT4;
+		CalibCounter++;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;	//Start sample.
+		while (ADC10CTL1 & BUSY);	//Wait for sample to be done.
+		Value += ADC10MEM0;
+	}
+	CalValue_Y = Value/50;	//Average all samples to find the best zero.
+	
+	ADC10CTL0 &= ~ADC10ENC;	//Toggle ENC bit. Need this to change the ADC10INCH_x value.
+	CalibCounter = 0;	//Reset the counters
+	Value = 0;
+	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_14;	//Sampel the Z-axis 
+	while(CalibCounter <50)
+	{
+		P3OUT ^= BIT4;
+		CalibCounter++;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;	//Start sample.
+		while (ADC10CTL1 & BUSY);  	//Wait for sample to be done.
+		Value += ADC10MEM0;
+	}
+	CalValue_Z = Value/50;	//Average all samples to find the best zero.
+	
+	ADC10CTL0 &= ~ADC10ENC;	//Toggle ENC bit. Need this to change the ADC10INCH_x value.
+	ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;	//We need to start at the X-axis first due to how the interupt routine works.
+	
+	__enable_interrupt();	//enable interupts
 }
 
-void SetupAccel()
-{
-
+void setupAccel(void)
+{  
 	//Setup  accelerometer
 	// ~20KHz sampling
 	//Configure GPIO
@@ -247,43 +108,80 @@ void SetupAccel()
   	ADC10IV = 0x00;		//Clear all ADC12 channel int flags  
   	ADC10IE |= ADC10IE0;	//Enable ADC10 interrupts
   	
-  	__enable_interrupt();
-  	
+  	__enable_interrupt(); 	
 }
 
-/****
-	Interrupt voids
-*/
-
-// Switch presses causes this debounce ISR to fire
-#pragma vector = TIMER1_A0_VECTOR
-__interrupt void Timer1_A0_ISR(void)
+//this is copied from the Ti Fraunch Pad experience. 
+//Only thing it does is make a fancy LED sequence on boot.
+void startUpSequence(void)	
 {
-	TA1CCTL0 = 0;
-	TA1CTL = 0;
-	EnableSwitches();
+	unsigned char flag=4,up=1,counter = 0; 
+	unsigned char LED_ArrayPJ[] = {0x01,0x02,0x04,0x08};
+	unsigned char LED_ArrayP3[] = {0x80,0x40,0x20,0x10};
+
+	while (counter <10) 
+	{    
+		counter++;
+		PJOUT &= ~(BIT0 +BIT1+BIT2+BIT3);
+		P3OUT &= ~(BIT4 +BIT5+BIT6+BIT7);    
+		if(up)
+		{
+			while(flag)
+			{
+				P3OUT = LED_ArrayP3[flag-1];
+				PJOUT = LED_ArrayPJ[flag-1];
+				LongDelay();               
+				flag--;
+			}
+			up=0;
+		}
+		else
+		{
+			while(flag<4)
+			{
+				P3OUT = LED_ArrayP3[flag];
+				PJOUT = LED_ArrayPJ[flag];
+				LongDelay();        
+				flag++;
+			}
+			up = 1;
+		}
+	}   
+	PJOUT &= ~(BIT0 +BIT1+BIT2+BIT3);
+	P3OUT &= ~(BIT4 +BIT5+BIT6+BIT7);    
 }
 
-// Port 4 ISR to detect switch presses
-#pragma vector = PORT4_VECTOR
-__interrupt void Port_4_ISR(void)
+void LongDelay()
 {
-	// See link to get information on __even_in_range intrinsic
-	// https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/238317
-	switch(__even_in_range(P4IV, P4IV_P4IFG1)) {
-		case P4IV_P4IFG0:
-			DisableSwitches();
-			P4IFG &= ~BIT0;					// Clear P4.0 IFG
-			PJOUT ^= BIT0;					// Toggle PJ.0
-			StartDebounceTimer(0);			// Reenable switches after debounce i.e. time elapsed
-			break;
-		case P4IV_P4IFG1:
-			DisableSwitches();
-			P4IFG &= ~BIT1;					// Clear P4.1 IFG
-			PJOUT ^= BIT1;					// Toggle PJ.1
-			StartDebounceTimer(0);			// Reenable switches after debounce i.e. time elapsed
-			break;
-	    default:
-	    	break;
+  __delay_cycles(250000);
+}
+
+//ADC10 interupt routine
+#pragma vector = ADC10_VECTOR
+__interrupt void ADC10_ISR(void)
+{   
+	if (ADC_counter == 0) //X-axis
+	{
+		ADC10CTL0 &= ~ADC10ENC;	
+		ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_13;	//Next channel is the Y-axis
+		ADCResult_X = ADC10MEM0;
+		ADC_counter++;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;
+	}
+	else if (ADC_counter == 1)	//Y-axis
+	{
+		ADC10CTL0 &= ~ADC10ENC;
+		ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_14;	//Next channel is the Z-axis
+		ADCResult_Y = ADC10MEM0;
+		ADC_counter++;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;
+	}
+	else	//Z-axis
+	{
+		ADC10CTL0 &= ~ADC10ENC;
+		ADC10MCTL0 = ADC10SREF_0 + ADC10INCH_12;	//Next channel is the X-axis
+		ADCResult_Z = ADC10MEM0;
+		ADC_counter = 0;
+		ADC10CTL0 |= ADC10ENC | ADC10SC;
 	}
 }
--- Ex2/Ex2.main/system.h
+++ Ex2/Ex2.main/system.h
@@ -1,20 +1,5 @@
 #ifndef SYSTEM_H
-#define SYSTEM_H
-#include <msp430fr5739.h>
-
-// Type definitions
-typedef unsigned char uc;
-typedef enum {FALSE, TRUE} bool;
-typedef unsigned char uc_8;
-typedef unsigned int uc_16;
-
-//variables
-//
-
-// User definitions
-#define LED_TOG_DLY_CONST	1000
-#define SW_DEB_CONST		1600			// Load into TA1CCR0 the value SW_DEB_CONST i.e. debounce time = 1600*1/8000 = 200ms
-#define SW_HALF_DEB_CONST	SW_DEB_CONST/2	// Shorter debounce time model assuming half
+#define SYSTEM_H 
 
 // Pin Definitions
 #define ACC_PWR_PIN       BIT7
@@ -29,35 +14,14 @@ typedef unsigned int uc_16;
 #define ACC_Z_PIN         BIT2
 
 // Accelerometer Input Channel Definitions
-#define ACC_X_CHANNEL ADC10INCH_12
-#define ACC_Y_CHANNEL ADC10INCH_13
-#define ACC_Z_CHANNEL ADC10INCH_14
+#define ACC_X_CHANNEL     ADC10INCH_12
+#define ACC_Y_CHANNEL     ADC10INCH_13
+#define ACC_Z_CHANNEL     ADC10INCH_14
 
-//Clock init
-void clockInit();
-//LEDs init
-void ledsInit(uc_8 ledNumber);
-//LEDs Write
-bool ledsWrite(uc_8 ledNumber, uc_8 value);
-//Switches init
-void switchesInit(uc_8 buttonNumber);
-//Switches Read
-bool switchesRead(uc_8 buttonNumber, uc_8 *value);
-
-void SetupThermistor(void);
-void ShutDownTherm(void);
-
-//Enable/Disable Switches
-inline void EnableSwitches();
-inline void DisableSwitches();
-inline void StartDebounceTimer(uc ucDelay);
-
-void CalibrateADC(void);
-
-void acceInit();
-
-void SetupAccel();
-
-
+void systemInit(void);
+void calibrateADC(void); 
+void setupAccel(void); // setup ADC for Accel
+void startUpSequence(void);
+void LongDelay(void);
 
 #endif
