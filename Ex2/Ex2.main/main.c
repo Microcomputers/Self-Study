a938fe129a80ce3a7b4d34741d1ade9ed1ae8675
Amir <amir_rajabifar@yahoo.com.au>
2016-11-04 12:02:10 +0800

    Rever back Ex2 main.c

--- Ex2/Ex2.main/main.c
+++ Ex2/Ex2.main/main.c
@@ -1,25 +1,40 @@
+#include <stdio.h>
 #include <msp430fr5739.h>
 #include "system.h"
-#include <stdio.h>
+#include "adc.h"
+//#include "uart.h"
 /*
- * main.c
+ 	main.c
+	1. Switches and LEDs
+	2.
+	3. Temp sens
  */
+#define led1Pin 4
+#define led2Pin 3
+#define button1Pin 1
+#define button2Pin 2
 
-extern unsigned int ADCResult_X, ADCResult_Y, ADCResult_Z;
-
-
+unsigned int ADCTResult_t, ADCTResult_x, ADCTResult_y, ADCTResult_z;
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	systemInit();				// setup pins
-	startUpSequence();			// Copy Ti's fancy LED boot up trick
-	setupAccel();				//Setup ADC nad Accel
-	calibrateADC();
-	//If this is not done the ADC10 interupt will not trigger.
-	ADC10CTL0 |= ADC10ENC | ADC10SC; //Start the first sample.
+	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+	ledsInit(led1Pin); 				// Init leds
+	ledsInit(led2Pin);
+	switchesInit(button1Pin); 			// Init Switches
+	switchesInit(button2Pin);
+	SetupThermistor();			// Init Temp sensor
+	CalibrateADC();				// Calibrate analog to digital convertor
+	//Start the first sample. If this is not done the ADC10 interupt will not trigger.
+	ADC10CTL0 |= ADC10ENC | ADC10SC;
+	uc_8 buttonState = 0; //buton status
 	while (1)
 	{
-		printf("%s%d\t%s%d\t%s%d\n", "X = ", (int)ADCResult_X, "Y = ", (int)ADCResult_Y, "Z = ", (int)ADCResult_Z);
+		switchesRead(button1Pin, &buttonState);
+		ledsWrite(led1Pin, buttonState);
+		switchesRead(button2Pin, &buttonState);
+		ledsWrite(led2Pin, buttonState);
+		//getThermisterVal();
+		printf("%s%d\t%s%d\t%s%d\t%s%d\n", "Temp = ", (int)ADCTResult_t, "X = ", (int)ADCTResult_x, "Y = ", (int)ADCTResult_y, "Z = ", (int)ADCTResult_z);
 	}
 	return 0;
 }
